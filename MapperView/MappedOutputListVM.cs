1371253858e224497e4011a4d06cb5a39995d786
Will L <willyplehman@gmail.com>
2017-07-20 08:45:28 -0700

    updating the reading of the outputs database to include a try catch

--- MapperView/MappedOutputListVM.cs
+++ MapperView/MappedOutputListVM.cs
@@ -9,40 +9,52 @@ namespace MapperView
     public class MappedOutputListVM
     {
         public System.Collections.ObjectModel.ObservableCollection<MappedOutputVM> Items { get; set; }
+        public bool WasSuccessful = false;
         public MappedOutputListVM(string outputDatabase)
         {
-            DataBase_Reader.SQLiteReader reader = new DataBase_Reader.SQLiteReader(outputDatabase);
-            reader.SetTableReader("SimulationRunLog");
-            reader.Open();
-            int[] indexes = new int[6];
-            string[] colNames = reader.ColumnNames;
-            indexes[0] = Array.IndexOf(colNames, "Representation");
-            indexes[1] = Array.IndexOf(colNames, "Name");
-            indexes[2] = Array.IndexOf(colNames, "HPSAlternative");
-            indexes[3] = Array.IndexOf(colNames, "LowSLCSelected");
-            indexes[4] = Array.IndexOf(colNames, "IntermediateSLCSelected");
-            indexes[5] = Array.IndexOf(colNames, "HighSLCSelected");
-            string seaLevelChangeScenario = "";
-            object[] row;
-            Items = new System.Collections.ObjectModel.ObservableCollection<MappedOutputVM>();
-            for(int i =0; i < reader.NumberOfRows; i++)
+            
+            try
             {
-                row = reader.GetRow(i, indexes);
-                if((bool)row[3])
-                {
-                    seaLevelChangeScenario = "Low";
-                }
-                else if((bool)row[4])
-                {
-                    seaLevelChangeScenario = "Intermediate";
-                }
-                else
-                {
-                    seaLevelChangeScenario = "High";
-                }
 
-                Items.Add(new MappedOutputVM((string)row[0], (string)row[2], (string)row[1], seaLevelChangeScenario));
+                DataBase_Reader.SQLiteReader reader = new DataBase_Reader.SQLiteReader(outputDatabase);
+                reader.SetTableReader("SimulationRunLog");
+                reader.Open();
+                int[] indexes = new int[6];
+                string[] colNames = reader.ColumnNames;
+                indexes[0] = Array.IndexOf(colNames, "Representation");
+                indexes[1] = Array.IndexOf(colNames, "Name");
+                indexes[2] = Array.IndexOf(colNames, "HPSAlternative");
+                indexes[3] = Array.IndexOf(colNames, "LowSLCSelected");
+                indexes[4] = Array.IndexOf(colNames, "IntermediateSLCSelected");
+                indexes[5] = Array.IndexOf(colNames, "HighSLCSelected");
+                string seaLevelChangeScenario = "";
+                object[] row;
+                Items = new System.Collections.ObjectModel.ObservableCollection<MappedOutputVM>();
+                for (int i = 0; i < reader.NumberOfRows; i++)
+                {
+                    row = reader.GetRow(i, indexes);
+                    if ((bool)row[3])
+                    {
+                        seaLevelChangeScenario = "Low";
+                    }
+                    else if ((bool)row[4])
+                    {
+                        seaLevelChangeScenario = "Intermediate";
+                    }
+                    else
+                    {
+                        seaLevelChangeScenario = "High";
+                    }
+
+                    Items.Add(new MappedOutputVM((string)row[0], (string)row[2], (string)row[1], seaLevelChangeScenario));
+                }
+                WasSuccessful = true;
             }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Errors opening " + outputDatabase + "\n" + ex.Message.ToString());
+            }
+
         }
     }
 }
